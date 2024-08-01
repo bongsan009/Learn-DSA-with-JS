54143262816685c07449cc483ed850faf8038392
Bunsan <bbek2930@gmail.com>
2024-08-01 20:48:35 +0700

    solve the problem number 2

--- Recursive/ans.js
+++ Recursive/ans.js
@@ -15,3 +15,22 @@ function factorial(num)
 // Test the function
 const n = 5;
 console.log(factorial(n))
+
+
+// ** 2. Find GCD
+function GCD(a, b)
+{
+    //Base case: when b equal to 0, then the the GCD is a.
+    if(!b)
+    {
+        return a;
+    }
+
+    //Calc the remainder of a and b and add it as new b.
+    const rem = (a % b);
+
+    return GCD(b, rem)
+}
+
+// Testing
+console.log(GCD(100, 5))
